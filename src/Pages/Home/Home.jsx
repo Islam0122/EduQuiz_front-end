6180cbf0e25697cd68de1ff6ef4a2afa88f0b4cd
nurhan004 <sharshenbekov777@gmail.com>
2025-02-02 19:09:35 +0600

    and block3

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Header from '../../UI/Header/Header'
 import Login from "../Login/Login";
 import HeroGreenting from "./HeroGreenting/HeroGreenting";
 import Footer from '../../UI/Footer/Footer'
+import Section3 from './Section3/Section3';
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
      <Header/>
      <HeroBlock/>
      <HeroGreenting/>
+     <Section3/>
 <Footer/>
      </div>
   )
