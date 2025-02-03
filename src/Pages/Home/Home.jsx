a9b72727b8dcc4a5587d3f0822c5544b7a5dcebd
windvenx <windvenx@mail.ru>
2025-02-03 14:44:32 +0600

    fix conflicts

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,23 +2,15 @@ import HeroBlock from './HeroBlock/HeroBlock'
 import Login from "../Login/Login";
 import HeroGreenting from "./HeroGreenting/HeroGreenting";
 import Section3 from './HomeAdvantages/HomeAdvantages';
+import HomeAdvantages from './HomeAdvantages/HomeAdvantages';
 
 const Home = () => {
   return (
-<<<<<<< HEAD
      <>
-     <Header/>
      <HeroBlock/>
      <HeroGreenting/>
-     <Footer/>
+     <HomeAdvantages/>
      </>
-=======
-     <div>
-     <HeroBlock/>
-     <HeroGreenting/>
-     <Section3/>
-     </div>
->>>>>>> master
   )
 }
 
