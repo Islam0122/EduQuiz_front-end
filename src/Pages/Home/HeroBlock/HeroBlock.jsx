03bfc51844b2f880bb076618443da5db2b52e433
windvenx <windvenx@mail.ru>
2025-02-06 14:48:51 +0600

    fix conflicts

--- src/Pages/Home/HeroBlock/HeroBlock.jsx
+++ src/Pages/Home/HeroBlock/HeroBlock.jsx
@@ -4,22 +4,6 @@ import Carousel from "../Carousel/Carousel";
 const HeroBlock = () => {
   return (
       <div className="container">
-<<<<<<< HEAD
-          <div className="HeroBlock-main">
-              <div className="nusoiba">
-                  <div className="HeroBlock1">
-                      <h1 className={"HeroBlock2"}>Добро пожаловать в систему викторин DevTrivia
-                      </h1>
-                      <p className={"p"}>Создавайте, проводите и анализируйте викторины в удобном формате. Наша
-                          платформа поможет вам проверять знания студентов, оценивать прогресс и делать обучение более
-                          интерактивным.
-                      </p>
-                      <div className="HeroButton">
-                          <button className={"HeroBtn1"}>Начать тестирование</button>
-                          <button className={"HeroBtn2"}>Читать далее</button>
-                      </div>
-                  </div>
-=======
           <div className="Greeting">
               <div className="greeting-description">
                   <h1 className={"text-box"}>Добро пожаловать в систему викторин DevTrivia
@@ -29,13 +13,12 @@ const HeroBlock = () => {
               <div className="Button">
                   <button className={"start-test"}>Начать тестирование</button>
                   <button className={"read-more"}>Читать далее</button>
->>>>>>> master
               </div>
               <div className="nusoiba-left">
                   <Carousel/>
               </div>
           </div>
-
+        </div>
       </div>
   )
 }
