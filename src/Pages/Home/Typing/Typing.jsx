a6f54966d76440bb4fbe3ff430531b23eca6a8b9
islam0122 <duishobaevislam01@gmaul.com>
2025-02-21 00:06:00 +0600

    fix css  ->  modals in  homepage

--- src/Pages/Home/Typing/Typing.jsx
+++ src/Pages/Home/Typing/Typing.jsx
@@ -5,21 +5,21 @@ import {useNavigate} from "react-router-dom";
 const Typing = () => {
     const navigate = useNavigate();
     return (
-        <section className={'home__Typing'}>
-            <div className={"container"}>
+        <section className='home__Typing'>
+            <div className="container">
                 <div className="Typing-flex">
-                    <div className={"Typing-images"}>
-                        <img className={"typing-img"} src="/images/typing.png" alt=""/>
+                    <div className="Typing-images">
+                        <img className="typing-img" src="/images/typing.png" alt=""/>
                     </div>
-                    <div className={'Typing-text'}>
-                        <h2 className={"text1"}>
+                    <div className='Typing-text'>
+                        <h2 className="text1">
                             Быстрая и точная печать — ваш ключ к продуктивности!
                         </h2>
-                        <p className={"text2"}>
+                        <p className="text2">
                             Освойте слепую печать — лучший метод набора текста без взгляда на клавиатуру. Это сэкономит время и повысит вашу эффективность.
-                            📈 Регулярные тренировки помогут развить скорость и точность. Используйте специальные тренажёры, практикуйтесь каждый день — и печать станет лёгкой и естественной!
+                            📈 Используйте специальные тренажёры, практикуйтесь каждый день — и печать станет лёгкой и естественной!
                         </p>
-                        <button onClick={() => navigate('/')} className={"btn"}>
+                        <button onClick={() => navigate('/')} className="btn">
                             Перейти  в “Typing”
                         </button>
                     </div>
