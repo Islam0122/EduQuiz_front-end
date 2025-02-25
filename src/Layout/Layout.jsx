640f8638b305840267534505abccc1f9aacac416
islam0122 <duishobaevislam01@gmaul.com>
2025-02-25 06:42:34 +0600

    fix -> bugs -> layoat

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,19 +1,24 @@
-// src/components/Layout.js
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 
 const Layout = () => {
-  return (
-    <>
-        <Header/>
-      <main>
-        <Outlet /> 
-      </main>
-        <Footer/>
-    </>
-  );
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return (
+        <>
+            <Header />
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
+        </>
+    );
 };
 
 export default Layout;
