03feca1b7a17a45b3455eaa13fa9c81791f40d28
islam0122 <duishobaevislam01@gmaul.com>
2025-02-27 15:24:07 +0600

    add -> typing page 5%

---
+++ src/Pages/Typing/Typing.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Typing = () => {
+    return (
+        <div className="flex justify-center items-center h-screen bg-gray-100">
+            <h1>typing</h1>
+        </div>
+    );
+};
+
+export default Typing;
