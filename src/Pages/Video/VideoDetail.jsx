617dfb04d8362fe14287c04b66f0a67fa7e30954
islam0122 <duishobaevislam01@gmaul.com>
2025-03-17 02:38:36 +0600

    fix css -> video page

--- src/Pages/Video/VideoDetail.jsx
+++ src/Pages/Video/VideoDetail.jsx
@@ -1,17 +1,21 @@
 import YouTube from "react-youtube";
-import { useNavigate, useParams } from "react-router-dom";
-import { useGetVideoByIdQuery, useGetVideosByCategoryQuery } from "../../redux/videoApi";
-import { Swiper, SwiperSlide } from "swiper/react";
+import {useNavigate, useParams} from "react-router-dom";
+import {useGetVideoByIdQuery, useGetVideosByCategoryQuery} from "../../redux/videoApi";
+import {Swiper, SwiperSlide} from "swiper/react";
 import "swiper/css";
 import "./VideoDetail.scss";
+import {FaEdit, FaInfoCircle, FaListUl, FaReact, FaThList, FaVideo} from "react-icons/fa";
+import {IoMdInformationCircle} from "react-icons/io";
+import React from "react";
+import {MdOutlineQuiz} from "react-icons/md";
 
 const VideoDetail = () => {
     const navigate = useNavigate();
-    const { id } = useParams();
-    const { data, error, isLoading } = useGetVideoByIdQuery(id);
+    const {id} = useParams();
+    const {data, error, isLoading} = useGetVideoByIdQuery(id);
     const category = data?.video_category_info?.title;
-    const { data: relatedVideos, error: videosError, isLoading: videosLoading } =
-        useGetVideosByCategoryQuery(category, { skip: !category });
+    const {data: relatedVideos, error: videosError, isLoading: videosLoading} =
+        useGetVideosByCategoryQuery(category, {skip: !category});
 
     if (isLoading || videosLoading) return <div className="loading-spinner">⏳ Загрузка...</div>;
     if (error || videosError) return (
@@ -34,15 +38,35 @@ const VideoDetail = () => {
         <section className="VideoDetail">
             <div className="container">
                 <div className="video">
-                    <YouTube className="youtube" videoId={videoId} />
+                    <div>
+                        <YouTube className="youtube" videoId={videoId}/>
+                    </div>
                     <div className="description">
-                        <h2>{data.title}</h2>
-                        <p>{data.description}</p>
+                        <h2>
+                            <FaVideo style={{color: "#ff9800", marginRight: "15px"}}/>
+                            {data.title}
+                        </h2>
+                        <p>
+                            <FaListUl style={{marginRight: '15px', color: '#3498db'}}/>
+                            {data.description}
+                        </p>
                     </div>
                 </div>
                 {category && relatedVideos?.length > 0 && (
                     <div className="category-video">
-                        <h3>Видео по категории {category}</h3>
+                        <h3>
+                            <FaThList
+                                style={{
+                                    marginRight: '15px',
+                                    color: '#FFD700',
+                                    transition: 'color 0.3s ease'
+                                }}
+                                onMouseEnter={(e) => e.target.style.color = '#FF6347'} // Цвет при наведении
+                                onMouseLeave={(e) => e.target.style.color = '#FFD700'} // Вернуться к изначальному цвету
+                            />
+                                Посмотрите еще полезные видео, связанные с "{category}"
+                        </h3>
+
                         <Swiper
                             spaceBetween={100}
                             slidesPerView={3}
@@ -63,7 +87,7 @@ const VideoDetail = () => {
                                                 loading="lazy"
                                             />
                                         </div>
-                                        <h2>{video.title}</h2>
+                                        <h2 className="h2">{video.title}</h2>
                                     </div>
                                 </SwiperSlide>
                             ))}
